358b55993843eb5ab1d5e5cfc2eb296e928643a5
Kaushik <86108909+kaushik-geyyer@users.noreply.github.com>
2022-12-29 19:13:51 +0530

    Update +page.svelte

--- frame2/frame2/src/routes/+page.svelte
+++ frame2/frame2/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+
+</script>
+
+<main>
+
+    <div class="nav-bar">
+        <div class="nav-bar-left"></div>
+        <div class="nav-bar-right"></div>
+    </div>
+
+    <div class="report">
+        <div class="rpt-summary"></div>
+        <div class="result-items"></div>
+    </div>
+
+</main>
